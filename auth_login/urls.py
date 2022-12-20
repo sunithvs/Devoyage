bf482b1a0d79ea1a9378c65a2f37813402665e22
sunithvs <sunithvs2002@gmail.com>
2022-12-21 00:23:59 +0530

    completed jwt token authentication

--- auth_login/urls.py
+++ auth_login/urls.py
@@ -6,16 +6,16 @@ from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 
 router = routers.DefaultRouter()
-router.register('api/users', UserAPI, 'users')
-router.register('api/register', RegisterAPI, 'register')
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('api/token/',
+    path('login/',
          jwt_views.TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path('api/token/refresh/',
+    path('token/refresh/',
          jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
+    path('auth/register', RegisterAPI.as_view()),
+    path('auth/user', UserAPI.as_view()),
 
 ]
