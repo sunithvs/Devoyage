5fc2976ac6f25b7912127e12ce12fbf850f1f0dc
sunithvs <sunithvs2002@gmail.com>
2022-12-21 19:53:41 +0530

    bug fix

--- university/admin.py
+++ university/admin.py
@@ -1,4 +1,37 @@
 from django.contrib import admin
-from .models import University
+from .models import University, Feedback, Course, Gallery, Image
 
-admin.site.register(University)
+
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'nirf_rank', 'n_u_p', 'affiliations', 'facilities']
+    list_filter = ['name', 'nirf_rank', 'n_u_p', 'affiliations', 'facilities']
+    search_fields = ['name', 'nirf_rank', 'n_u_p', 'affiliations', 'facilities']
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['user', 'heading', 'message', 'date']
+    list_filter = ['user', 'heading', 'message', 'date']
+    search_fields = ['user', 'heading', 'message', 'date']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'image', 'duration', 'intake']
+    list_filter = ['name', 'description', 'image', 'duration', 'intake']
+    search_fields = ['name', 'description', 'image', 'duration', 'intake']
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
+    list_filter = ['name', ]
+    search_fields = ['name', ]
